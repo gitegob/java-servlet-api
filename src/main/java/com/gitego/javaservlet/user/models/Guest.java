c9ee7af9140ea34bf65a25f61a1a6ef13a161593
gbrian <gitegob7@gmail.com>
2022-04-02 11:30:13 +0200

    init

--- src/main/java/com/gitego/javaservlet/user/models/Guest.java
+++ src/main/java/com/gitego/javaservlet/user/models/Guest.java
@@ -2,6 +2,7 @@ package com.gitego.javaservlet.user.models;
 
 import com.gitego.javaservlet.db.Database;
 import com.gitego.javaservlet.user.dtos.ApiResponse;
+import com.gitego.javaservlet.user.dtos.Role;
 
 import javax.naming.AuthenticationException;
 import java.util.regex.Pattern;
@@ -9,7 +10,13 @@ import java.util.regex.Pattern;
 public class Guest extends User {
     @Override
     public ApiResponse<User> register() throws Exception {
-        if (!Pattern.matches("^(?=.*[0-9])(?=.*[a-zA-Z])(?=\\S+$).{5,}$", this.getPassword())) {
+        if(Database.findUser(this.getEmail())!=null){
+            throw new Exception("User already exists");
+        }
+        String regex = "";
+        if(this.getRole()== Role.ADMIN) regex = "^(?=.*[0-9])(?=.*[a-zA-Z])(?=\\S+$).{10,}$";
+        else regex = "^(?=.*[0-9])(?=.*[a-zA-Z])(?=\\S+$).{5,}$";
+        if (!Pattern.matches(regex, this.getPassword())) {
             throw new Exception("Password rules not met");
         }
 
